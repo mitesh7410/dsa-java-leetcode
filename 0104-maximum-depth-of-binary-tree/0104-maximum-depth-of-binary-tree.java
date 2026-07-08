64795667204c3d6439958ceb7154bd01ca6aa00d
mitesh7410 <mitesh2022saini@gmail.com>
2026-07-08 13:14:18 +0530

    Time: 0 ms (100%), Space: 46.9 MB (7.78%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -15,9 +15,13 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
+       return Height(root);
+    }
+    public static int Height(TreeNode root){
         if(root==null) return 0;
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-        return Math.max(left,right)+1;
+        int left = Height(root.left);
+        int right = Height(root.right);
+        int height = Math.max(left,right)+1;
+        return height;
     }
 }
