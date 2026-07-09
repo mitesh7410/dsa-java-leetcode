6ecc1a8b0e6c2f1ca702655e78eee644613bd17a
mitesh7410 <mitesh2022saini@gmail.com>
2026-07-09 13:53:51 +0530

    Time: 0 ms (100%), Space: 42 MB (59.29%) - LeetHub

--- 0556-next-greater-element-iii/0556-next-greater-element-iii.java
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.java
@@ -1,42 +1,35 @@
 class Solution {
     public int nextGreaterElement(int n) {
-        List<Integer> li = new LinkedList<>();
-        int num = n;
-        while(num>0){
-        int rem = num%10;
-          li.addFirst(rem);
-          num/=10;
-        }
-        int len = li.size()-2;
-        while(len>=0&&li.get(len)>=li.get(len+1))
-        len--;
-        if(len==-1) return -1;
-        for(int i=li.size()-1;i>len&&len>=0;i--){
-            if(li.get(i)>li.get(len)){
-             swap(li,len,i);
-             break;
-            }
-        }
-          return reverse(li,len+1,li.size()-1);
-      
-    }
-    private static void swap(List<Integer> li,int i, int j){
-        int temp = li.get(i);
-        li.set(i,li.get(j));
-        li.set(j,temp);
-    }
-    private static int reverse(List<Integer> li,int i, int j){
-        while(i<j){
-            swap(li,i,j);
-            i++;
+        char[] digits = String.valueOf(n).toCharArray();
+        int len = digits.length;
+
+   
+        int i = len - 2;
+        while (i >= 0 && digits[i] >= digits[i+1])
+            i--;
+
+        if (i == -1) return -1;  
+
+        int j = len - 1;
+        while (digits[j] <= digits[i])
             j--;
-        }
-        int a =0;
-        long num=0;
-        while(a<li.size()){
-           num = num*10 + li.get(a);
-           a++;
-        }
-        return num>Integer.MAX_VALUE? -1:(int)num;
+
+        swap(digits, i, j);
+
+        reverse(digits, i+1, len-1);
+
+        long result = Long.parseLong(new String(digits));
+        return result > Integer.MAX_VALUE ? -1 : (int) result;
+    }
+
+    private void swap(char[] arr, int i, int j) {
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    private void reverse(char[] arr, int l, int r) {
+        while (l < r)
+            swap(arr, l++, r--);
     }
 }
