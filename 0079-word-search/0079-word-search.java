469865aa97b4520b71c5c7a9b546cca4f5678c1d
mitesh7410 <mitesh2022saini@gmail.com>
2026-07-14 11:23:30 +0530

    Time: 131 ms (68.71%), Space: 43 MB (59.97%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int row = board.length;
+        int col = board[0].length;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(search(board,word,0,i,j))
+                 return true;
+            }
+        }
+        return false;
+    }
+   private boolean search(char[][] board,String word, int idx, int r, int c){
+        if(idx==word.length()) return  true;
+        if(r<0||r>=board.length||c<0||c>=board[0].length||word.charAt(idx)!=board[r][c])
+        return false;
+
+        char temp = board[r][c];
+        board[r][c]='#';
+        boolean found=search(board,word,idx+1,r,c+1)||search(board,word,idx+1,r+1,c)||search(board,word,idx+1,r,c-1)||search(board,word,idx+1,r-1,c);
+        
+        board[r][c] =temp;
+        return found;
+    }
+}
