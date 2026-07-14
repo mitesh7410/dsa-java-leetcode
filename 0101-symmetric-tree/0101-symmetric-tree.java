0470760b0260e41863bec76b86962b840fea022a
mitesh7410 <mitesh2022saini@gmail.com>
2026-07-14 18:30:39 +0530

    Time: 0 ms (100%), Space: 43.6 MB (53.02%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -1,39 +1,12 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if(root.left==null&&root.right==null)return true;
-        if(root.left!=null&&root.right==null||root.right!=null&&root.left==null)return false;
-        
-        TreeNode temp = invert(root.left);
-        return check(temp,root.right);    
+        if (root == null) return true;
+        return check(root.left, root.right);
     }
-    private TreeNode invert(TreeNode temp){
-      if(temp==null) return null;
-        TreeNode t  =temp.left;
-        temp.left=temp.right;
-        temp.right=t;
-        invert(temp.left);
-        invert(temp.right);
-      
-        return  temp;
-    }
-    private boolean check(TreeNode  l, TreeNode r){
-        if(l==null&&r==null)return true;
-        if(l==null||r==null||l.val!=r.val) return false;
-        return check(l.left,r.left)&&check(l.right,r.right);
+
+    private boolean check(TreeNode l, TreeNode r) {
+        if (l == null && r == null) return true;
+        if (l == null || r == null || l.val != r.val) return false;
+        return check(l.left, r.right) && check(l.right, r.left);
     }
 }
