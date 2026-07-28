b7c2fd76e8a2635dec2ae3e68c9fc2635ae1dd86
mitesh7410 <mitesh2022saini@gmail.com>
2026-07-28 21:30:07 +0530

    Time: 31 ms (63.05%), Space: 48.3 MB (38.15%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -1,18 +1,29 @@
 class Solution {
     public String smallestPalindrome(String s) {
-        if(s.length()==1) return s;
-        int len = s.length()/2;
-        String str = s.substring(0,len);
-        char[] ch = str.toCharArray();
-        Arrays.sort(ch);
-        StringBuilder st = new  StringBuilder();
-        for(char c:ch)
-               st.append(c);
-        if(s.length()%2!=0) 
-          st.append(s.charAt(len));     
-        for(int i=ch.length-1;i>=0;i--)
-          st.append(ch[i]);
+        int n = s.length();
+        int freq[] = new int[26];
+        for (int i = 0; i < n; i++) {
+            char curr = s.charAt(i);
+            freq[curr - 'a']++;
+        }
+        StringBuilder sb = new StringBuilder();
+        char mid = 'a';
+        for (int i = 0; i < 26; i++) {
+            char c = (char) ('a' + i);
+            for (int j = 0; j < freq[i] / 2; j++) {
+                sb.append(c);
+            }
+            if (freq[i] % 2 == 1) {
+                mid = c;
+            }
+        }
         
-        return st.toString();  
+        String second = new StringBuilder(sb).reverse().toString();
+
+        if (n % 2 == 1)
+            sb.append(mid);
+
+        sb.append(second);
+        return sb.toString();
     }
 }
