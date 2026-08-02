13acdd41ae632189969f57ae2d550c07fb77fcd9
mitesh7410 <mitesh2022saini@gmail.com>
2026-08-02 21:53:19 +0530

    Time: 6 ms (99.47%), Space: 44.9 MB (43.06%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,34 +1,46 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int maxleft=0,maxright=0;
-        int n = s.length();
-        for(int i=0;i<n;i++){
-            int left=i;int right=i;
-            while((left>=0&&right<n)&&(s.charAt(left)==s.charAt(right))){
-                left--;
-                right++;
-            }
-            left++;
-            right--; 
-            if((right)-(left)>maxright-maxleft)
-            {   
-                maxleft=left;
-                maxright=right;
-            }
-              left=i;right=i+1;
-            while((left>=0&&right<n)&&(s.charAt(left)==s.charAt(right))){
-                left--;
-                right++;
-            }
-            left++;
-            right--; 
-            if((right)-(left)>maxright-maxleft)
-            {   
-                maxleft=left;
-                maxright=right;
-            }
-            
+        // Transform s into t with separators
+        // "bab" → "^#b#a#b#$"
+        StringBuilder sb = new StringBuilder("^#");
+        for (char c : s.toCharArray()) {
+            sb.append(c);
+            sb.append('#');
         }
-        return s.substring(maxleft,maxright+1);
+        sb.append('$');
+        String t = sb.toString();
+        int n = t.length();
+
+        int[] p = new int[n];  // p[i] = radius of palindrome at i
+        int center = 0, right = 0;
+
+        for (int i = 1; i < n - 1; i++) {
+            int mirror = 2 * center - i;
+
+            if (i < right)
+                p[i] = Math.min(right - i, p[mirror]);
+
+            // expand around i
+            while (t.charAt(i + p[i] + 1) == t.charAt(i - p[i] - 1))
+                p[i]++;
+
+            // update center and right boundary
+            if (i + p[i] > right) {
+                center = i;
+                right = i + p[i];
+            }
+        }
+
+        // find max in p
+        int maxLen = 0, centerIndex = 0;
+        for (int i = 1; i < n - 1; i++) {
+            if (p[i] > maxLen) {
+                maxLen = p[i];
+                centerIndex = i;
+            }
+        }
+
+        int start = (centerIndex - maxLen - 1) / 2;
+        return s.substring(start, start + maxLen);
     }
 }
