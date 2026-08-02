c0f65d3d0ecd939e5e9ac0b7122f43b8fe8b9de2
mitesh7410 <mitesh2022saini@gmail.com>
2026-08-02 13:43:50 +0530

    Time: 0 ms (100%), Space: 43.1 MB (62.03%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,24 +1,5 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        int alice =0;
-        boolean al = true;
-       
-        int bob =0;
-        int left=0,right = piles.length-1;
-        while(left<right){
-              if(al){
-                if(piles[left]>=piles[right])
-                  alice+=piles[left++];
-                else
-                  alice+=piles[right--];
-              }
-              else{
-                if(piles[left]<piles[right])
-                  bob+=piles[left++];
-                else
-                  bob+=piles[right--];
-              }
-        }
-        return alice>bob;
+       return true;
     }
 }
