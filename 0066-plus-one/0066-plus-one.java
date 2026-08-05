ffdd57958459835707cb5329047c5fa15e96fad1
mitesh7410 <mitesh2022saini@gmail.com>
2026-08-05 20:57:14 +0530

    Time: 0 ms (100%), Space: 43.4 MB (43.36%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,23 +1,14 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-         List<Integer> ans = new LinkedList<>();
-         int carry=1;
-         for (int i = digits.length - 1; i >= 0; i--) {
-            int sum = digits[i] + carry;
-            ans.addFirst(sum % 10); 
-            carry = sum / 10;             
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
         }
-        if (carry > 0) {
-            ans.addFirst(carry);
-        }
-        
-        int[] result = new int[ans.size()];
-
-        for(int i=0;i<ans.size();i++)
-         result[i]=ans.get(i);
-
-
-        return result; 
-       
+        int[] ans = new int[digits.length + 1];
+        ans[0] = 1;
+        return ans;
     }
 }
