7be5bab3fa63fe57e5ab511e5f9494d1aca64cf6
mitesh7410 <mitesh2022saini@gmail.com>
2026-08-08 13:23:24 +0530

    Time: 9 ms (94.93%), Space: 49 MB (59.1%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int INF = Integer.MAX_VALUE/2;
+        int[][] dist =  new int[m][n];
+        for(int i=0;i<m;i++){ 
+            for(int j=0;j<n;j++){
+                if(mat[i][j]!=0)
+                   dist[i][j] = INF;
+            }
+        }
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i>0) dist[i][j] = Math.min(dist[i][j],dist[i-1][j]+1);
+                if(j>0) dist[i][j] = Math.min(dist[i][j],dist[i][j-1]+1);
+
+            }
+        }
+
+         for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(i<m-1) dist[i][j] = Math.min(dist[i][j],dist[i+1][j]+1);
+                if(j<n-1) dist[i][j] = Math.min(dist[i][j],dist[i][j+1]+1);
+
+            }
+        }
+
+        return dist;
+    }
+}
