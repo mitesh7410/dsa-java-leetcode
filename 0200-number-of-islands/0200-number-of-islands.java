79054192f5cd1e5c14f0adef9e59e8caa6ef547a
mitesh7410 <mitesh2022saini@gmail.com>
2026-08-13 20:38:28 +0530

    Time: 2 ms (99.86%), Space: 52.5 MB (22.01%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,25 +1,24 @@
 class Solution {
-    private void dfs(char[][] grid, boolean[][] visited, int i, int j){
-        if(i<0||i>=grid.length||j<0||j>=grid[0].length||visited[i][j]==true||grid[i][j]=='0')
+    private void dfs(char[][] grid, int i, int j){
+        if(i<0||i>=grid.length||j<0||j>=grid[0].length||grid[i][j]=='0')
         return;
 
-        visited[i][j]=true;
+        grid[i][j]='0';
 
-        dfs(grid,visited,i+1,j);
-        dfs(grid,visited,i-1,j);
-        dfs(grid,visited,i,j+1);
-        dfs(grid,visited,i,j-1);
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
 
     }
     public int numIslands(char[][] grid) {
         int island = 0;
         int m = grid.length;
         int n = grid[0].length;
-        boolean visited[][] = new boolean[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]=='1'&&visited[i][j]==false){
-                    dfs(grid,visited,i,j);
+                if(grid[i][j]=='1'){
+                    dfs(grid,i,j);
                     island++;
                 }
             }
