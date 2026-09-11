df388fb4fd96d049d23b18ca238153e575a4da80
mitesh7410 <mitesh2022saini@gmail.com>
2026-09-11 13:18:57 +0530

    Time: 4 ms (48.7%), Space: 46.6 MB (49.13%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] arr = new int[26]; 
+        int count=0;
+        for(char ch : ransomNote.toCharArray()){
+           arr[ch-'a']++;
+        }
+        for(char ch: magazine.toCharArray() ){
+            arr[ch-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(arr[i]>0) return false;
+        }
+        return true;
+    }
+}
