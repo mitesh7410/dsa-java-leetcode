27d9eb60eabd75795585b0ac2362753fdb3bdabd
mitesh7410 <mitesh2022saini@gmail.com>
2026-09-22 20:55:45 +0530

    Time: 15 ms (61.17%), Space: 43.3 MB (95.1%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,46 +1,36 @@
 class Solution {
     public String longestPalindrome(String s) {
-        // Transform s into t with separators
-        // "bab" → "^#b#a#b#$"
-        StringBuilder sb = new StringBuilder("^#");
-        for (char c : s.toCharArray()) {
-            sb.append(c);
-            sb.append('#');
-        }
-        sb.append('$');
-        String t = sb.toString();
-        int n = t.length();
-
-        int[] p = new int[n];  // p[i] = radius of palindrome at i
-        int center = 0, right = 0;
-
-        for (int i = 1; i < n - 1; i++) {
-            int mirror = 2 * center - i;
-
-            if (i < right)
-                p[i] = Math.min(right - i, p[mirror]);
-
-            // expand around i
-            while (t.charAt(i + p[i] + 1) == t.charAt(i - p[i] - 1))
-                p[i]++;
-
-            // update center and right boundary
-            if (i + p[i] > right) {
-                center = i;
-                right = i + p[i];
+        int left = 0;
+        int right = 0;
+        int n = s.length();
+        for(int i = 0; i<n; i++){
+            int l = i;
+            int r = i;
+            while(l>=0&&r<n&&s.charAt(l)==s.charAt(r)){
+                l--;
+                r++;
             }
-        }
-
-        // find max in p
-        int maxLen = 0, centerIndex = 0;
-        for (int i = 1; i < n - 1; i++) {
-            if (p[i] > maxLen) {
-                maxLen = p[i];
-                centerIndex = i;
+            l++;
+            r--;
+            if(r-l>right-left){
+                right = r;
+                left = l;
             }
-        }
+            l=i;
+            r=i+1;
+            while(l>=0&&r<n&&s.charAt(l)==s.charAt(r)){
+                l--;
+                r++;
+            }
+            l++;
+            r--;
+            if(r-l>right-left){
+                right = r;
+                left = l;
+            }
 
-        int start = (centerIndex - maxLen - 1) / 2;
-        return s.substring(start, start + maxLen);
+        }
+        return s.substring(left,right+1);
     }
+
 }
