98c9e6292f77efe92daddb7a2189fd71988096e7
mitesh7410 <mitesh2022saini@gmail.com>
2026-09-26 18:17:02 +0530

    Time: 2 ms (81.97%), Space: 46.5 MB (64.08%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,53 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> ans = new ArrayList<>();
+        char[][] arr = new char[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j]='.';
+            }
+        }
+        placeQueen(0,ans,arr);
+        return ans;
+    }
+    private void placeQueen(int row, List<List<String>> ans, char[][] arr){
+        if(row==arr.length){
+            ans.add(construct(arr));
+            return;
+        }
+        for(int i=0;i<arr.length;i++){
+            if(isSafe(arr,row,i)){
+               arr[row][i]='Q';
+               placeQueen(row+1,ans,arr);
+               arr[row][i]='.';
+            }
+        }
+    }
+    private List<String> construct(char[][]arr){
+        List<String> str = new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
+            String row = new String(arr[i]);
+            str.add(row);
+        }
+        return str;
+    }
+    private boolean isSafe(char[][]arr,int row, int col){
+        
+        for(int i=0;i<row;i++){
+            if(arr[i][col]=='Q')
+             return false;
+        }
+        int left = Math.min(row,col);
+        for(int i=1;i<=left;i++){
+            if(arr[row-i][col-i]=='Q')
+            return false;
+        }
+        int right = Math.min(row,arr.length-1-col);
+
+        for(int i=1;i<=right;i++){
+            if(arr[row-i][col+i]=='Q')
+            return false;
+        }
+        return true;
+    }
+}
