8a93218b7b9c7e6dd964253618e9870142cbe97a
mitesh7410 <mitesh2022saini@gmail.com>
2026-09-26 07:25:35 +0530

    Time: 31 ms (96.83%), Space: 97.7 MB (19.46%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
@@ -1,31 +1,27 @@
 class Solution {
     public String evaluate(String s, List<List<String>> knowledge) {
-         HashMap<String,String> map = new HashMap<>();
-         int n = knowledge.size();
-         for(int i=0;i<n;i++){
-            List<String> li = knowledge.get(i);
-            map.put(li.get(0),li.get(1));
-         }
-         n = s.length();
-         StringBuilder ans = new StringBuilder();
-         for(int i=0;i<n;i++){
-            if(s.charAt(i)=='('){
-                StringBuilder str = new StringBuilder();
-                i++;
-                while(s.charAt(i)!=')'){
-                    str.append(s.charAt(i));
-                    i++;
-                }
-                String key =str.toString();
-                if(map.containsKey(key)){
-                    ans.append(map.get(key));
-                }else{
-                    ans.append('?');
-                }
-                
-            }else
-            ans.append(s.charAt(i));
-         }
-         return ans.toString();
+        Map<String, String> knowledgeMap = new HashMap<>(knowledge.size());
+      
+        for (List<String> pair : knowledge) {
+            knowledgeMap.put(pair.get(0), pair.get(1));
+        }
+      
+        StringBuilder result = new StringBuilder();
+      
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                int closingBracketIndex = s.indexOf(')', i + 1);
+              
+                String key = s.substring(i + 1, closingBracketIndex);
+              
+                result.append(knowledgeMap.getOrDefault(key, "?"));
+              
+                i = closingBracketIndex;
+            } else {
+                result.append(s.charAt(i));
+            }
+        }
+      
+        return result.toString();
     }
 }
